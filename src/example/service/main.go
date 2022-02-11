5b1af12c4b69d0ad735de46a693a5951d5a0c973
Shawn Tabai <stabai@gmail.com>
2022-02-11 14:07:14 -0800

    feat: add reflection-only grpc server and rerun gazelle

---
+++ src/example/service/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+func main() {
+	flag.Parse()
+
+	log.Println("Initializing server...")
+	initializeServer()
+}
+
+func initializeServer() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	registerGrpcServices(s)
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func registerGrpcServices(s *grpc.Server) {
+	reflection.Register(s)
+	// pb.RegisterPublicWorkSuggestionServiceServer(s, &service{Db: db})
+}
