d8e459ec958b9629066e09287d7f878d5e704fa1
Shawn Tabai <stabai@gmail.com>
2022-02-11 21:51:11 -0800

    docs: add comment explaining tidy.sh

--- tidy.sh
+++ tidy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# This script updates Bazel dependencies for imports to Go code using Gazelle.
+# Run this whenever you would normally run `go mod tidy`.
 
 go mod tidy
 bazel run //:gazelle-update-repos
