ddeab8e9b75d6dde5f9ad89a0e100d52a10b1cb0
Shawn Tabai <stabai@gmail.com>
2022-05-09 00:57:10 -0700

    Add helm and vscodium to nix shell

--- default.nix
+++ default.nix
@@ -1,5 +1,39 @@
-{ pkgs ? import <nixpkgs> {}
-}:
+let
+  pkgs = import <nixpkgs> {};
+  lib = import <nixpkgs/lib>;
+  extensions = (with pkgs.vscode-extensions; [
+    bbenoist.nix
+    golang.go
+    redhat.vscode-yaml
+    zxh404.vscode-proto3
+    editorconfig.editorconfig
+    ms-azuretools.vscode-docker
+    ms-kubernetes-tools.vscode-kubernetes-tools
+  ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {
+      name = "Tiltfile";
+      publisher = "tilt-dev";
+      version = "0.0.3";
+      sha256 = "KQ+jmVwHH/9iqmS6mYGxZMMmVisI2FqMMCDUi9AJCcY=";
+    }
+    {
+      name = "vscode-bazel";
+      publisher = "BazelBuild";
+      version = "0.5.0";
+      sha256 = "JJQSwU3B5C2exENdNsWEcxFSgWHnImYas4t/KLsgTj4=";
+    }
+    {
+      name = "vscode-buf";
+      publisher = "bufbuild";
+      version = "0.5.0";
+      sha256 = "ePvmHgb6Vdpq1oHcqZcfVT4c/XYZqxJ6FGVuKAbQOCg=";
+    }
+  ];
+  vscode-with-extensions = pkgs.vscode-with-extensions.override {
+    vscode = pkgs.vscodium;
+    vscodeExtensions = extensions;
+  };
+in
 pkgs.mkShell {
   name="dev-environment";
   buildInputs = [
@@ -10,13 +44,19 @@ pkgs.mkShell {
     pkgs.go
     pkgs.golangci-lint
     pkgs.kubectl
+    pkgs.kubernetes-helm
     pkgs.minikube
     pkgs.tilt
+    vscode-with-extensions
   ];
   shellHook = ''  # commands to run when you enter the shell
     alias groot="git rev-parse --show-toplevel"
     alias cdg="cd $(groot)"
+    alias code="codium"
+    alias codeg="code $(groot)"
     alias tidy="$(groot)/tidy.sh"
+    alias k="kubectl"
+    alias mk="minikube"
     echo
     echo "$(tput bold)Development environment ready!$(tput sgr0)"
     echo
